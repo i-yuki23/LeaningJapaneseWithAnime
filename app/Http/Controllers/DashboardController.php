75174decf58a7a5c52b05dd2d5a2bdeccd1827fa
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-06-01 18:38:23 +0900

    create user page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -10,5 +11,13 @@ class DashboardController extends Controller
     public function index() {
         $posts = Auth::user()->posts()->latest()->paginate(6);
         return view('users.dashboard', ['posts' => $posts]);
+    }
+
+    public function userPosts(User $user) {
+        $usrPosts = $user->posts()->latest()->paginate(6);
+        return view('users.posts', [
+            'posts' => $usrPosts,
+            'user' => $user
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ Route::redirect('/', '/posts');
 
 Route::resource('posts', PostController::class);
 
+Route::get('/{user}/posts', [DashboardController::class, 'userPosts'])->name('posts.user');
+
 // Only authenticated users can access these routes
 Route::middleware(('auth'))->group(function() {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
