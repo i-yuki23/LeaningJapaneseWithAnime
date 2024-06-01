9e8e54804cec417266833ff7961db824f5320921
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-06-01 21:50:42 +0900

    create single post page

--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -1,11 +1,18 @@
-@props(['post'])
+@props(['post', 'full' => false])
 <div class="card mb-4">
     <h2 class="font-bold text-xl">{{ $post->title }}</h2>
     <div class="text-xs font-light mb-4">
         <span>Posted {{ $post->created_at->diffForHumans() }} By</span>
         <a href="{{ route('posts.user', $post->user) }}" class="text-blue-500 font-medium">{{ $post->user->username }}</a>
     </div>
-    <div class="text-sm">
-        <p>{{ Str::words($post->body, 30) }}</p>
-    </div>
+    @if ($full)
+        <div class="text-sm">
+            <span>{{$post->body}}</span>
+        </div>    
+    @else
+        <div class="text-sm">
+            <span>{{ Str::words($post->body, 30) }}</span>
+            <a href="{{ route('posts.show', $post)}}" class="text-blue-500 ml-2">Read more &rarr;</a>
+        </div>
+    @endif
 </div>
